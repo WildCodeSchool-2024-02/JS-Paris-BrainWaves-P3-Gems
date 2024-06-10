6e096da0786a06352fa9179ebf2ef1096405fc7b
maximmft <mmaufront@gmail.com>
2024-06-10 17:25:12 +0200

    router done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,35 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import App from "./App";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/loginPage", element: <LoginPage /> },
-      { path: "/createAccount", element: <CreateAccount /> },
-      { path: "/itemsPage", element: <ItemsPage /> },
-      {
-        path: "/itemsDetailsPage",
-        element: <ItemDetailsPage />,
-      },
-      {
-        path: "/addToCart",
-        element: <AddToCart/>,
-      },
-      { path: "/sellingPage", element: <SellingPage /> },
-      { path: "/profilePage", element: <ProfilePage /> },
-      { path: "/adminPage", element: <AdminPage /> },
-
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ client/src/router.jsx
@@ -0,0 +1,39 @@
+// router.jsx
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import CreateAccount from "./components/pages/CreateAccount";
+import ItemsPage from "./components/pages/ItemsPage";
+import ItemDetailsPage from "./components/pages/ItemDetailsPage";
+import AddToCart from "./components/pages/AddToCart";
+import SellingPage from "./components/pages/SellingPage";
+import ProfilePage from "./components/pages/ProfilePage";
+import AdminPage from "./components/pages/AdminPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/loginPage", element: <LoginPage /> },
+      { path: "/createAccount", element: <CreateAccount /> },
+      { path: "/itemsPage", element: <ItemsPage /> },
+      {
+        path: "/itemsDetailsPage",
+        element: <ItemDetailsPage />,
+      },
+      {
+        path: "/addToCart",
+        element: <AddToCart />,
+      },
+      { path: "/sellingPage", element: <SellingPage /> },
+      { path: "/profilePage", element: <ProfilePage /> },
+      { path: "/adminPage", element: <AdminPage /> },
+    ],
+  },
+]);
+
+export default router;
