5624b7bd17f0a880e35025d620d5c130fac50118
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-06-18 10:32:47 +0200

    user update

---
+++ server/database/models/UserRepository.js
@@ -0,0 +1,16 @@
+const AbstractRepository = require("./AbstractRepository");
+
+class UserRepository extends AbstractRepository {
+  constructor() {
+    super({ table: "user" });
+  }
+
+  async readProduct() {
+    const [rows] = await this.database.query(
+      `select * from ${this.table} INNER JOIN product ON ${this.table}.Id_user = product.Id_user`
+    );
+    return [rows];
+  }
+}
+
+module.exports = UserRepository;
