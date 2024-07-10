40fe4d5f4610c16d883ff215bd5ff42c266f07e1
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-07-10 12:08:11 +0200

    merging branch dev

--- client/src/components/pages/ItemsPage/ItemsPage.jsx
+++ client/src/components/pages/ItemsPage/ItemsPage.jsx
@@ -3,17 +3,17 @@ import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import "./ItemsPage.css";
 import { useEffect } from "react";
 import Card from "../../Card/Card";
-import Flower from "../../../assets/images/illustrations/flower2.png"
-import Flower2 from "../../../assets/images/illustrations/flower1.jpg"
-import Flower3 from "../../../assets/images/illustrations/flower2bis.png"
+import Flower from "../../../assets/images/illustrations/flower2.png";
+import Flower2 from "../../../assets/images/illustrations/flower1.jpg";
+import Flower3 from "../../../assets/images/illustrations/flower2bis.png";
 import { useCart } from "../../../contexts/CartContext";
 
 function ItemsPage() {
   const location = useLocation();
   const displayProduct = location.state;
   const navigate = useNavigate();
-  const { favorites, setFavorites } = useOutletContext();  
-  const {cart, setCart} = useCart();
+  const { favorites, setFavorites } = useOutletContext();
+  const { cart, setCart } = useCart();
 
   useEffect(() => {
     const initialLocalCart = localStorage.getItem("cart");
@@ -29,29 +29,38 @@ function ItemsPage() {
   return (
     <div id="ItemsPage">
       <h2>{displayProduct[0].title.toUpperCase()}</h2>
-      <MdOutlineKeyboardBackspace onClick={() => navigate(-1)} style={{ marginLeft: "20px" }} />
+      <MdOutlineKeyboardBackspace
+        onClick={() => navigate(-1)}
+        style={{ marginLeft: "20px" }}
+      />
       <div className="the-filter">
         <div>
-          <button type="button" >Prix croissant</button>
-          <button type="button" >Prix décroissant</button>
+          <button type="button">Prix croissant</button>
+          <button type="button">Prix décroissant</button>
         </div>
         <h3>FILTRER PAR</h3>
       </div>
       <div className="container-items">
         {displayProduct.map((product, index) => (
           <>
-          <Card key={product.Id_product} product={product} cart={cart}
-            setCart={setCart}  favorites={favorites} setFavorites={setFavorites}/>
-          {(index + 1) % 6 === 0 && (
+            <Card
+              key={product.Id_product}
+              product={product}
+              cart={cart}
+              setCart={setCart}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+            {(index + 1) % 6 === 0 && (
               <img
-                src={(Math.floor(index / 6) % 2 === 0) ? Flower : Flower2}
+                src={Math.floor(index / 6) % 2 === 0 ? Flower : Flower2}
                 alt="Flower"
                 className="illustrations-mobile"
               />
             )}
             {(index + 1) % 8 === 0 && (
               <img
-                src={(Math.floor(index / 8) % 2 === 0) ? Flower2 : Flower3}
+                src={Math.floor(index / 8) % 2 === 0 ? Flower2 : Flower3}
                 alt="Flower"
                 className="illustrations-desktop"
               />
