502a0b8be84753f28e622eb1181118dac429b976
Micka90 <156587591+Micka90@users.noreply.github.com>
2024-07-17 09:59:28 +0200

    Update AdminRoute.jsx

--- client/src/components/auth/AdminRoute.jsx
+++ client/src/components/auth/AdminRoute.jsx
@@ -6,7 +6,7 @@ function AdminRoute({ children }) {
 
   if (!auth || !auth.user.is_admin) {
     return <div>Accès refusé. Vous n'avez pas les droits nécessaires pour accéder à cette page.</div>;
-
+}
   return children;
 }
 
